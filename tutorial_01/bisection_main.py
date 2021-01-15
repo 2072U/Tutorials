208fab613e87fda93d4082601882e39da7dcede6
Parikshit Bajpai <parikshit.bajpai@ontariotechu.net>
2021-01-14 20:44:04 -0500

    Tutorial 1 starter code

---
+++ tutorial_01/bisection_main.py
@@ -0,0 +1,15 @@
+# CSCI / MATH 2072U -- Computational Science 1
+# Date: 1/13/2021
+# Tutorial 1, Part B
+#                    GNU GENERAL PUBLIC LICENSE
+#                       Version 3, 29 June 2007
+
+
+import bisection
+
+def func(x):  # Function for bisection.
+    # Add function
+
+print("--Bisection--\n")
+    # Call Bisection using Recursion
+print ("Solution is " + + " +/- " + + " with residual " + ) # Add the missing variables.
---
+++ tutorial_01/iteration.py
@@ -0,0 +1,28 @@
+# CSCI / MATH 2072U -- Computational Science 1
+# Date: 1/13/2021
+# Tutorial 1, Part A
+#                    GNU GENERAL PUBLIC LICENSE
+#                       Version 3, 29 June 2007
+
+
+# Implementation of the iterative computation of a square root.
+# In: x, initial point; a, parameter; k_max, max nr of iterations
+# Out: x, approximation of sqrt(a)
+def iteration(x, a, k_max):
+
+    y =                                 # Apply map to evaluate the current approximation
+
+    print ("Current approximation = " + str(y))
+
+    # Iterate to find successive approximations
+
+    return                              # Decide the function to return
+
+# Map that produces iterates converging to sqrt(a)
+def map(x, a):
+    # Add code
+
+# Execution part of code
+print("Fixed point iteration")
+        #  Call the function with suitable values
+print ("Solution is " + )               # Add the value to be printed
