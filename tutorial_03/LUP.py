e572d38041c78b2dd07e450e69231fba9d67aafd
Parikshit Bajpai <parikshit.bajpai@ontariotechu.net>
2021-02-02 14:34:12 -0500

    Minor modifications

--- tutorial_03/LUP.py
+++ tutorial_03/LUP.py
@@ -11,32 +11,25 @@ from copy import copy
 
 # Note: indices i,j and k are used as matrix indeces (starting from 1) while s is used as python array index (starting from 0)
 def LUP(A):
-    small = 1e-12                                 # a pivot smaller than this will raise the error flag "ok = 0"
     n = scipy.shape(A)[0]                         # extract matrix size
     U = copy(A)                                   # copy content of A (avoid linking U and A)
     L = scipy.identity(n)                         # initialize L and P
     P = scipy.identity(n)
-    par = 1                                       # initial permutation (identity) is even
-    ok = 1                                        # by default, we assume the matrix is non-singular
+
     for k in range(# Decide the range):
         # find pivot element
-        if #condition                             # check if pivot is too close to zero
-            # Your code to handle pivot close to zero
 
-
-        if #condition                             # if the pivot is not on the diagonal...
-            # change parity
+        if #condition:                            # if the pivot is not on the diagonal...
             # swap rows of U
-            if #condition                         # swap rows of L left of diagonal element
+            if #condition:                        # swap rows of L left of diagonal element
                 # Your code goes here
-
-
             # swap rows of P
-        for j in range(# Decide the range):               # Gauss elimination of rows below pivot
+        for j in range(# Decide the range):       # Gauss elimination of rows below pivot
             # Your code goes here
 
 
-    return L,U,P,par,ok
+    return L,U,P
+
 
 def swap(M,i,j,k):
 # Swap rows i and j from column 0 up to (but not including) k
